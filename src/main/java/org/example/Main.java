064bd76c6ad135e838a97d2f0e09e632383f1b4b
Zvonimir Belina <zvona.belina52@gmail.com>
2025-11-11 10:07:40 +0100

    Add first iteration of command validation which the user writes.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -8,8 +9,8 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome to Task Tracker, an app that allows you to track your tasks and manage your to-do list!\n");
-        System.out.println("Type 'help' to see the list of available commands!");
+        System.out.println("Hello and welcome to Task Tracker, an app that allows you to track your tasks and manage your to-do list!");
+        System.out.println("Type 'help' to see the list of available commands!\n");
 
         Scanner scanner = new Scanner(System.in);
 
@@ -17,10 +18,11 @@ public class Main {
             System.out.print("Enter your command: ");
             String command = scanner.nextLine().trim();
 
-            System.out.println("Command: " + command);
+            System.out.println("Command: " + command); //Remove
+            checkCommand(command);
 
             if (command.equals("Q")) {
-                System.out.println("End of program.");
+                System.out.println("Exiting application.");
                 break;
             } else if(command.equals("help")){
                 printHelp();
@@ -28,6 +30,34 @@ public class Main {
         }
         scanner.close();
     }
+
+    private static void checkCommand(String command) {
+        String[] parts = command.split("\\s+", 2);
+        String mainCommand = parts[0];
+
+        List<String> validCommands = List.of(
+                "add", "update", "delete",
+                "mark-in-progress", "mark-done",
+                "list", "help", "Q"
+        );
+
+        if (!validCommands.contains(mainCommand)) {
+            System.out.println("Invalid command. Type 'help' to see the list of available commands.");
+            return;
+        }
+
+        // If "list" has an invalid subcommand → show message
+        if (mainCommand.equals("list")) {
+            if (parts.length > 1) {
+                String sub = parts[1].trim();
+                List<String> validSubcommands = List.of("done", "todo", "in-progress");
+                if (!validSubcommands.contains(sub)) {
+                    System.out.println("Invalid list option. Valid options are: done, todo, in-progress.");
+                }
+            }
+        }
+    }
+
 
     public static void printHelp() {
         String helpText = """
